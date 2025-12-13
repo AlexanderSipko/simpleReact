c1dc5aa0bc08786fbc77b3c7c90df76699ae02a6
asipko <avsipko@gmail.com>
2025-12-13 21:16:09 +0300

    create | создан новый репозиторий

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import { Button } from 'antd';
+import { TableData } from './elements/table';
+
+
+const App = () => (
+  <div className="App">
+    <Button type="primary">Hello Ant Design Button</Button>
+
+    <TableData></TableData>
+  </div>
+);
+
+export default App;
+
---
+++ src/elements/table/index.jsx
@@ -0,0 +1,11 @@
+import { Table } from "antd"
+
+function TableData () {
+
+
+    return <Table>
+
+    </Table>
+}
+
+export { TableData }
